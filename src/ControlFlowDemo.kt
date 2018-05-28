b20e50356e12379b7c0ba0b1ac9e854ea1b4cf03
xhh4215 <18734184215@163.com>
2018-05-28 17:30:04 +0800

    break continue return返回和跳转的关键字的使用

--- src/ControlFlowDemo.kt
+++ src/ControlFlowDemo.kt
@@ -2,6 +2,7 @@ package mydemo.kotlin
 
 import javafx.beans.binding.When
 import org.omg.CORBA.INTERNAL
+import java.util.*
 
 /***
  * @author 栾桂明
@@ -13,6 +14,13 @@ fun main(args: Array<String>) {
     IfTest3()
     WhenTest1(12)
     WhenTest2("string 字符串")
+    ForTest1()
+    ForTest2()
+    ForTest3()
+    ReturnAndBreakTest1()
+    ReturnAndBreakTest2()
+
+
 }
 
 /***
@@ -66,5 +74,62 @@ fun WhenTest2(x: Any) = when (x) {
     }
     else -> {
         false
+    }
+}
+
+/***
+ * for 可以循环遍历任何提供了迭代器的对象。即：
+ *有一个成员函数或者扩展函数 iterator()，它的返回类型
+ *有一个成员函数或者扩展函数 next()，并且
+ *有一个成员函数或者扩展函数 hasNext() 返回 Boolean。
+ */
+fun ForTest1() {
+    for (item in 1..12) {
+        println("集合中的数据是$item")
+    }
+}
+
+fun ForTest2() {
+    val array = arrayOf("这是什么呀", "这是一个数组呀", "那好的我们看看数组有什么元素")
+    for (i in array.indices) {
+        println("数值中的第$i 个元素是 " + array[i])
+
+    }
+}
+
+fun ForTest3() {
+    val array = arrayOf("这是什么呀", "这是一个数组呀", "那好的我们看看数组有什么元素")
+    for ((index, value) in array.withIndex()) {
+        println("数值中的第$index 个元素是 " + value)
+
+    }
+}
+
+/***
+ * break 的作用是整个循环的操作
+ */
+fun ReturnAndBreakTest1() {
+    val array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    for (item in array) {
+        if (item == 5) {
+            break
+        } else {
+            println("集合中的数据是$item")
+        }
+    }
+}
+
+/****
+ * continue 的作用是停止某一次循环
+ * return 从包围他的函数或者是你匿名函数返回
+ */
+fun ReturnAndBreakTest2() {
+    val array = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    for (item in array) {
+        if (item == 5) {
+            continue
+        } else {
+            println("集合中的数据是$item")
+        }
     }
 }
